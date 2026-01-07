9977b9800f983546ed2fc769cf0987b271eb6723
snehakalracodingal-ctrl <snehakalra.codingal@gmail.com>
2026-01-07 16:35:21 +0530

    first commit

---
+++ ACP.py
@@ -0,0 +1,11 @@
+bday1 = "12-05-2001"
+bday2 = "23-08-2000"
+bday3 = "04-01-2002"
+bday4 = "19-11-1999"
+bday5 = "30-06-2003"
+
+print("Birthday 1:", bday1)
+print("Birthday 2:", bday2)
+print("Birthday 3:", bday3)
+print("Birthday 4:", bday4)
+print("Birthday 5:", bday5)
---
+++ activity1.py
@@ -0,0 +1,14 @@
+
+print("Welcome to the world of programming!")
+
+
+print(4)
+
+#Next Line after print
+print("Hello Python \n")
+
+#Pass multiple arguments for printing
+print("hello",5)
+
+#End argument of Print Statement
+print("welcome to ", end= "*")
